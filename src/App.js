2a1acbbb6a8d668d6b5935cbc618a78d8b0423fa
danielfasanaro <danielfasanaro@gmail.com>
2021-04-13 20:34:13 -0300

    Change Redux store to Context API state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,55 @@ import React from 'react';
 import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
+import TrafficContext from './TrafficContext';
 
-function App() {
-  return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+      signal: { color: 'red' },
+    };
+
+    this.moveCar = this.moveCar.bind(this);
+    this.changeSignal = this.changeSignal.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState((prevState) => ({
+      cars: {
+        ...prevState.cars,
+        [car]: side,
+      }
+    }));
+  }
+
+  changeSignal(color) {
+    this.setState({
+      signal: { color },
+    })
+  }
+
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      signal: this.state.signal,
+      moveCar: this.moveCar,
+      changeSignal: this.changeSignal,
+    };
+    return (
+      <TrafficContext.Provider value={ contextValue }>
+        <div className="container">
+          <Cars />
+          <TrafficSignal />
+        </div>
+      </TrafficContext.Provider>
+    );
+  }
 }
 
 export default App;
