fea0fbf32c37b6b2650654abaa5da879343e7021
danielfasanaro <danielfasanaro@gmail.com>
2021-04-13 19:24:52 -0300

    Change redux store to Context API state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import CarsContext from './CarsContext'
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState((prevState) => ({
+      cars: {
+        ...prevState.cars,
+        [car]: side,
+      }
+    }));
+  }
+
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      moveCar: this.moveCar,
+    }
+    return (
+      <CarsContext.Provider value={ contextValue } >
+        <Cars />
+      </CarsContext.Provider>
+    );
+  }
 }
 
 export default App;
